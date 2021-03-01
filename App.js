99d19ca67e9d29c9d7c9d65f9305c2867794e2b7
Anthony <t9haan01@students.oamk.fi>
2021-03-01 09:04:08 +0200

    Updated with root stack

--- App.js
+++ App.js
@@ -2,94 +2,37 @@ import React, { useState, useEffect } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import AuthStackNavigator from './navigators/AuthStackNavigaator';
+import MainStackNavigator from './navigators/MainStackNavigator';
+import AllStackNavigator from './navigators/AllStackNavigator';
 import { AuthContext } from './contexts/AuthContext';
-import axios from 'axios';
-import { BASE_URL } from './config';
-import { sleep } from './utils/sleep';
-import base64 from 'react-native-base64';
-import { createAction } from './utils/createAction';
+import { UserContext } from './contexts/UserContext';
+import SplashScreen from './screens/auth/SplashScreen';
+import useAuth from './hooks/useAuth';
 
 const RootStack = createStackNavigator();
-const AuthStack = createStackNavigator();
 
 export default function App() {
-  const [state, dispatch] = React.useReducer(
-    (state, action) => {
-      switch (action.type) {
-        case 'SET_USER':
-          return {
-            ...state,
-            user: {
-              ...action.payload,
-            },
-          };
-        default:
-          return state;
-      }
-    },
-    {
-      user: undefined,
-    }
-  );
-  const authContext = React.useMemo(
-    () => ({
-      login: async (username, password) => {
-        const authHeader = 'Basic ' + base64.encode(`${username}:${password}`);
-        const { data } = await axios({
-          method: 'post',
-          url: `${BASE_URL}/users/login`,
-          headers: { Authorization: authHeader },
-        });
-        const user = {
-          token: data.token,
-          userId: data.userId,
-        };
-        dispatch(createAction('SET_USER', user));
-      },
-      logout: () => {
-        console.log('logout');
-      },
-      register: async (
-        email,
-        username,
-        password,
-        firstname,
-        lastname,
-        street,
-        number,
-        postcode,
-        city,
-        country,
-        phone
-      ) => {
-        await sleep(1000);
-        await axios({
-          method: 'post',
-          url: `${BASE_URL}/users/signup`,
-          data: {
-            email: email,
-            username: username,
-            password: password,
-            name: {
-              firstname: firstname,
-              lastname: lastname,
-            },
-            address: {
-              street: street,
-              number: number,
-              postcode: postcode,
-              city: city,
-              country: country,
-            },
-            phone: phone,
-          },
-        });
-      },
-    }),
-    []
-  );
+  const { authContext, state } = useAuth();
 
-  console.log('state.user', state.user);
+  function renderScreens() {
+    if (state.loading) {
+      return <RootStack.Screen name={'Splash'} component={SplashScreen} />;
+    }
+    return state.user ? (
+      <RootStack.Screen name={'MainStack'}>
+        {() => (
+          <UserContext.Provider value={state.user}>
+            <MainStackNavigator />
+          </UserContext.Provider>
+        )}
+      </RootStack.Screen>
+    ) : (
+      <>
+        <RootStack.Screen name={'AllStack'} component={AllStackNavigator} />
+        <RootStack.Screen name={'AuthStack'} component={AuthStackNavigator} />
+      </>
+    );
+  }
 
   return (
     <AuthContext.Provider value={authContext}>
@@ -97,8 +40,9 @@ export default function App() {
         <RootStack.Navigator
           screenOptions={{
             headerShown: false,
+            animationEnabled: false,
           }}>
-          <RootStack.Screen name={'AuthStack'} component={AuthStackNavigator} />
+          {renderScreens()}
         </RootStack.Navigator>
       </NavigationContainer>
     </AuthContext.Provider>
